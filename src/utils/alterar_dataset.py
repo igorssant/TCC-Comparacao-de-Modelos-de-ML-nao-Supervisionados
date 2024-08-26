8ac77159c80dc3cb20c32512c9c55cf27ba3f1b4
Igor Sousa dos Santos Santana <igorssant@hotmail.com>
2024-08-26 14:50:39 -0300

    feat: finalizado pivoteamento e processamento dos datasets 'ureia' e 'creatinina'

--- src/utils/alterar_dataset.py
+++ src/utils/alterar_dataset.py
@@ -14,3 +14,23 @@ def preencher_dias_faltantes(df:pd.DataFrame, multi_indice:list|str, corpo_datas
     novo_dataframe.update(df.set_index(multi_indice))
     
     return novo_dataframe
+
+def pivotear_dataset_ureia(df_ureia:pd.DataFrame, coluna_indice:str, coluna_pivoteada:str, coluna_para_novos_valores:str) -> pd.DataFrame:
+    df_pivoteado = df_ureia.pivot(
+        index = coluna_indice,
+        columns = coluna_pivoteada,
+        values = coluna_para_novos_valores
+    )
+    df_pivoteado.columns = ["ureia_dia_" + str(coluna) for coluna in df_pivoteado.columns]
+
+    return df_pivoteado
+
+def pivotear_dataset_creatinina(df_creatinina:pd.DataFrame, coluna_indice:str, coluna_pivoteada:str, coluna_para_novos_valores:str) -> pd.DataFrame:
+    df_pivoteado = df_creatinina.pivot(
+        index = coluna_indice,
+        columns = coluna_pivoteada,
+        values = coluna_para_novos_valores
+    )
+    df_pivoteado.columns = ["creatinina_dia_" + str(coluna) for coluna in df_pivoteado.columns]
+
+    return df_pivoteado
