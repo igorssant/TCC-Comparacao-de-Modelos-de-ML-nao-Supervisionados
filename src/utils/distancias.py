88a62ece6b0b945ae63d395976fe6e7e16c303ad
Igor Sousa dos Santos Santana <igorssant@hotmail.com>
2024-09-09 19:18:06 -0300

    feat: implementando a correcao dos datasets 'ureia e creatinina' && fix: typo no arquivo DBSCAN

---
+++ src/utils/distancias.py
@@ -0,0 +1,29 @@
+# SE ESTIVER UTILIZANDO ESTA CLASSE DE MANEIRA DIRETA
+# EXCLUA AS LINHA 3 e 4
+#
+import sys
+sys.path.append("./utils/")
+from scipy.spatial.distance import pdist, squareform
+import pegarsubstring
+import pandas as pd
+
+def distancia_gower(x:pd.Series, y:pd.Series) -> list[float]:
+    tamanho = len(x)
+    distancia = 0
+    
+    for i in range(tamanho):
+        if pd.isnull(x[i]) or pd.isnull(y[i]):
+            distancia += 1
+            
+        elif isinstance(x[i], str) or isinstance(y[i], str):
+            distancia += int(x[i] != y[i])
+        
+        else:
+            distancia += abs(x[i] - y[i]) / max(abs(x[i]), abs(y[i]))
+    
+    return distancia / tamanho
+
+def ajustar_distancias(dataframe:pd.DataFrame) -> list[float]:
+    distances = pdist(dataframe, distancia_gower)
+    
+    return squareform(distances)
