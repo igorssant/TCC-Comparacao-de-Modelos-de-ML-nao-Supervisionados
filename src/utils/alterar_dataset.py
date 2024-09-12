8795e1f5a9f271d4364b6dc8d19bb16e06a2a0ec
Igor Sousa dos Santos Santana <igorssant@hotmail.com>
2024-09-12 08:32:51 -0300

    feat: transformando funcoes locais em funcoes globais

--- src/utils/alterar_dataset.py
+++ src/utils/alterar_dataset.py
@@ -2,9 +2,9 @@ import numpy as np
 import pandas as pd
 
 def preencher_dias_faltantes(df:pd.DataFrame, multi_indice:list|str, corpo_dataset:str, limite_dias:int) -> pd.DataFrame:
-    dataframe_agrupado = df.groupby(multi_indice[0])
+    dataframe_agrupado:pd.DataFrame = df.groupby(multi_indice[0])
     
-    novo_dataframe = pd.DataFrame(
+    novo_dataframe:pd.DataFrame = pd.DataFrame(
         index = pd.MultiIndex.from_product(
             [dataframe_agrupado.groups.keys(), range(1, limite_dias)],
             names = multi_indice
@@ -16,21 +16,17 @@ def preencher_dias_faltantes(df:pd.DataFrame, multi_indice:list|str, corpo_datas
     return novo_dataframe
 
 def pivotear_dataset_ureia(df_ureia:pd.DataFrame, coluna_indice:str, coluna_pivoteada:str, coluna_para_novos_valores:str) -> pd.DataFrame:
-    df_pivoteado = df_ureia.pivot(
-        index = coluna_indice,
-        columns = coluna_pivoteada,
-        values = coluna_para_novos_valores
-    )
+    df_pivoteado:pd.DataFrame = df_ureia.pivot(index = coluna_indice,
+                                               columns = coluna_pivoteada,
+                                               values = coluna_para_novos_valores)
     df_pivoteado.columns = ["ureia_dia_" + str(coluna) for coluna in df_pivoteado.columns]
 
     return df_pivoteado
 
 def pivotear_dataset_creatinina(df_creatinina:pd.DataFrame, coluna_indice:str, coluna_pivoteada:str, coluna_para_novos_valores:str) -> pd.DataFrame:
-    df_pivoteado = df_creatinina.pivot(
-        index = coluna_indice,
-        columns = coluna_pivoteada,
-        values = coluna_para_novos_valores
-    )
+    df_pivoteado:pd.DataFrame = df_creatinina.pivot(index = coluna_indice,
+                                                    columns = coluna_pivoteada,
+                                                    values = coluna_para_novos_valores)
     df_pivoteado.columns = ["creatinina_dia_" + str(coluna) for coluna in df_pivoteado.columns]
 
     return df_pivoteado
@@ -40,8 +36,12 @@ def despivotear_dataset(df:pd.DataFrame,
                         colunas_colapsar:list[str]|str,
                         nome_coluna_colapsada:str,
                         nome_coluna_de_valores:str) -> pd.DataFrame:
-    df_despivoteado = df.melt(id_vars = coluna_id,
-                              value_vars = colunas_colapsar,
-                              value_name = nome_coluna_colapsada,
-                              var_name = nome_coluna_de_valores)
+    df_despivoteado:pd.DataFrame = df.melt(id_vars = coluna_id,
+                                           value_vars = colunas_colapsar,
+                                           value_name = nome_coluna_colapsada,
+                                           var_name = nome_coluna_de_valores)
     return df_despivoteado
+
+def multi_indexar_dataset(df:pd.DataFrame, indices:str|list[str]) -> pd.DataFrame:
+    df_multi_indexado:pd.DataFrame = df.set_index(keys = ["subject_id", "day"])
+    return df_multi_indexado.sort_index()
--- src/utils/pegarsubstring.py
+++ src/utils/pegarsubstring.py
@@ -1,3 +1,7 @@
+import sys
+sys.path.append("./utils/")
+import pandas as pd
+
 def substring(frase:str, separacao:str= "= ") -> str:
      try:
           frase = frase.split(separacao, 1)[1]
@@ -7,3 +11,7 @@ def substring(frase:str, separacao:str= "= ") -> str:
           frase = ""
           
      return frase
+
+def renomear_series(serie:pd.Series, excluir:str) -> pd.Series:
+     nova_serie:pd.Series = [valor.replace(excluir, "") for valor in serie]
+     return nova_serie
