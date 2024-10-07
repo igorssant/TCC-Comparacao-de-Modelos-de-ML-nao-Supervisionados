d6b30d88e5176d83e70cf3a7c678f0322536c16e
Igor Sousa dos Santos Santana <igorssant@hotmail.com>
2024-10-07 15:31:24 -0300

    feat: nova tentativa de gerar imagens a partir do dataset repartido

--- src/utils/gerar_grafico.py
+++ src/utils/gerar_grafico.py
@@ -1,12 +1,13 @@
 import sys
 sys.path.append("./utils/")
 from matplotlib import pyplot as plt
+import seaborn as sns
 from PIL import Image
 import pandas as pd
 import io
 
 
-def __gerar_grafico(dados_paciente:pd.DataFrame, paciente:str) -> io.BytesIO:
+def __gerar_grafico_despivoteado(dados_paciente:pd.DataFrame, paciente:str) -> io.BytesIO:
     buffer: io.BytesIO
     
     plt.figure(figsize = (8, 6))
@@ -20,13 +21,27 @@ def __gerar_grafico(dados_paciente:pd.DataFrame, paciente:str) -> io.BytesIO:
     
     return buffer.getvalue()
 
-def gerar_dataframe_imagens_ureia(df:pd.DataFrame) -> pd.DataFrame:
+def __gerar_grafico_pivoteado(dados_paciente:pd.DataFrame, paciente:str) -> io.BytesIO:
+    buffer: io.BytesIO
+    
+    plt.figure(figsize = (8, 6))
+    sns.lineplot(dados_paciente)
+    plt.title(f"Paciente: {paciente}")
+    plt.xlabel("Dias")
+    plt.ylabel("Ureia")
+    buffer = io.BytesIO()
+    plt.savefig(buffer, format = "jpg")
+    plt.close()
+    
+    return buffer.getvalue()
+
+def gerar_dataframe_imagens_ureia_despivoteado(df:pd.DataFrame) -> pd.DataFrame:
     lista_imagens = []
     lista_pacientes:list[str] = df.index.to_list()
     
     for paciente in lista_pacientes:
         lista_imagens.append(
-            __gerar_grafico(df, paciente)
+            __gerar_grafico_despivoteado(df, paciente)
         )
     
     dados:dict = {
@@ -35,4 +50,19 @@ def gerar_dataframe_imagens_ureia(df:pd.DataFrame) -> pd.DataFrame:
     }
     
     return pd.DataFrame(dados)
+
+def gerar_dataframe_imagens_ureia_pivoteado(df:pd.DataFrame) -> pd.DataFrame:
+    lista_imagens = []
+    lista_pacientes:list[str] = df.index.to_list()
     
+    for paciente in lista_pacientes:
+        lista_imagens.append(
+            __gerar_grafico_pivoteado(df, paciente)
+        )
+    
+    dados:dict = {
+        "subject_id" : lista_pacientes,
+        "img_ureia" : lista_imagens
+    }
+    
+    return pd.DataFrame(dados)
